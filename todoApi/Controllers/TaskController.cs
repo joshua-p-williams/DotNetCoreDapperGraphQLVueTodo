861e9229656cca49793bfa7efcd99c105f71821a
Joshua Williams <joshua.p.williams@gmail.com>
2019-11-08 22:55:42 -0600

    Cleaned up using statements

--- todoApi/Controllers/TaskController.cs
+++ todoApi/Controllers/TaskController.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 using todoApi.Repositories;
 
 namespace todoApi.Controllers
